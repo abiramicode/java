8cad4154350bc37e4632b16c5b031250e8325d89
abiramikumaresan <34230155+abiramikumaresan@users.noreply.github.com>
2018-02-09 10:31:56 +0530

    Add files via upload

---
+++ demostudent.java
@@ -0,0 +1,16 @@
+class student
+{
+  public void getdata()
+	{
+	 System.out.println("hi");
+	}
+}
+
+class demostudent
+{
+   public static void main(String arsg[])
+	{
+	  student obj=new student();
+	  obj.getdata();	
+	}
+}
