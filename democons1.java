e9fd6c256f9a2d50b21e4bcfdc44b1e5a8da2a82
abiramikumaresan <34230155+abiramikumaresan@users.noreply.github.com>
2018-02-22 12:12:15 +0530

    Add files via upload

---
+++ democons1.java
@@ -0,0 +1,17 @@
+class sample
+{
+    sample(int x,int y)
+	{
+	  int add=x+y;
+	  System.out.println(""+add);	
+	}
+}
+
+class democons1
+{
+    public static void main(String args[])
+	{
+	  sample obj=new sample(12,14);
+	  sample obj1=new sample(25,15);
+	}
+}
