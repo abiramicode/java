002f9c202c173d14a6f82f41b5dfe28c7e377cfd
abiramikumaresan <34230155+abiramikumaresan@users.noreply.github.com>
2018-02-22 12:11:14 +0530

    Add files via upload

---
+++ democons.java
@@ -0,0 +1,16 @@
+class sample
+{
+    sample(int x, int y)
+	{
+	  int tot=x+y;
+	  System.out.println(""+tot);	
+	}
+}
+
+class democons
+{
+    public static void main(String args[])
+	{
+	  sample obj=new sample(10,12);
+	}
+}
