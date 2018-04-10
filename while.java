02cfb3186701108a650bfd152257f40362131af1
abiramikumaresan <34230155+abiramikumaresan@users.noreply.github.com>
2018-04-10 19:05:13 +0530

    Add files via upload

---
+++ while.java
@@ -0,0 +1,20 @@
+class sample
+{
+     int x=1;
+     sample()
+	{
+	  while(x<10);	
+	  {
+	  System.out.println(x);
+	  x++;
+	  }	
+      }
+}
+
+class while
+{  
+     public static void main(String args[])
+	{
+	 sample obj=new sample();
+	}
+}
