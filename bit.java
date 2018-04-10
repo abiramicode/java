8c194d729dbd96e56ba1389bc2c67321e2cbea03
abiramikumaresan <34230155+abiramikumaresan@users.noreply.github.com>
2018-04-10 19:04:05 +0530

    Add files via upload

---
+++ bit.java
@@ -0,0 +1,17 @@
+class sample
+{
+   int x=12,y=15;
+   sample()
+	{
+	      System.out.println(x|y);
+	}
+		
+}
+
+class bit
+{  
+   public static void main(String args[])
+	{
+	 sample obj=new sample();
+	}
+}
